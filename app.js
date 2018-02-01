689e0d88fcfcfe11ede7217aa26ef2b9401faf55
johansundstrom <johan.sundstrom@mdh.se>
2018-02-01 18:02:10 +0100

    static files

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
-    var id = req.query.n;   //Funkar
-    var n = req.body.n;     //funkar
-    res.send('<html><body><h1>Johan ' + id + '</h1><p>' + n + '</p><form><input name="n" type="text"><button type="submit">Skicka</button></form></body></html>');
+app.use('/media', express.static(__dirname + '/media'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api', function(req, res){
+    var id = req.query.id;   //Funkar
+    var eName = req.body.n;     //funkar
+    res.send('<html><body><p>[ <a href="/">start</a> | <a href="/api">post</a> | <a href="/api">get</a> ]</a></p><h1>Johan ' + id + '</h1><p>' + eName + '</p><form><input name="n" type="text"><button type="submit">Skicka</button></form></body></html>');
 });
 
 app.listen(3000);
