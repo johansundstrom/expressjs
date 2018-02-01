96dc5b4811aecddcee8fa2a2a0381a435450f9f4
Johan sundstrom <johansundstrom@users.noreply.github.com>
2018-02-01 17:18:36 +0100

    Add files via upload
    init

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api', function(req, res){
+    var id = req.query.n;   //Funkar
+    var n = req.body.n;     //funkar
+    res.send('<html><body><h1>Johan ' + id + '</h1><p>' + n + '</p><form><input name="n" type="text"><button type="submit">Skicka</button></form></body></html>');
+});
+
+app.listen(3000);
