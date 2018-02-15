a75fe747c06e7136d3e6238f173a539b62b076e9
johansundstrom <johan.sundstrom@mdh.se>
2018-02-15 17:49:50 +0100

    added media

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/api', function(req, res){
     var id = req.query.id;   //Funkar
     var eName = req.body.n;     //funkar
-    res.send('<html><body><p>[ <a href="/">start</a> | <a href="/api">post</a> | <a href="/api">get</a> ]</a></p><h1>Johan ' + id + '</h1><p>' + eName + '</p><form><input name="n" type="text"><button type="submit">Skicka</button></form></body></html>');
+    res.send('<html lang="en"><body><p>[ <a href="/">start</a> | <a href="/api">post</a> | <a href="/api">get</a> ]</a></p><h1>Johan ' + id + '</h1><p>' + eName + '</p><form><input name="n" type="text"><button type="submit">Skicka</button></form></body></html>');
 });
 
 app.listen(3000);
